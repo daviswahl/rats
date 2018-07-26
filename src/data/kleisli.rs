421f14231e96a5105d002db3cb109439e0d5c4fa
Davis Wahl <dwahl@signalpath.com>
2018-07-26 17:37:34 -0400

    add map

--- src/data/kleisli.rs
+++ src/data/kleisli.rs
@@ -1,18 +1,35 @@
+use functor::Functor;
 use lifted::Lifted;
 use monad::Monad;
 use std::boxed::FnBox;
 use std::marker::PhantomData;
+use std::rc::Rc;
 
 pub trait KleisliT<'a, F: 'static, A, B: 'a> {
     fn run(&self, a: A) -> Lifted<'a, F, B>;
 }
 
-trait KleisliExt<'a, F, A, B> {
+pub trait KleisliExt<'a, F, A, B>
+where
+    F: 'static,
+    B: 'a,
+    Self: Sized + KleisliT<'a, F, A, B>,
+{
     fn compose<Z, K>(self, k: K) -> Compose<'a, F, A, B, Z, Self, K>
     where
         F: Monad<'a, F>,
-        Self: Sized + KleisliT<'a, F, A, B>,
         K: KleisliT<'a, F, Z, A>;
+
+    fn map<Z, Func>(self, func: Func) -> Map<Z, Self, Func>
+    where
+        Func: Fn(B) -> Z,
+    {
+        Map {
+            func: RcFn(Rc::new(func)),
+            k: self,
+            marker: PhantomData,
+        }
+    }
 }
 
 impl<'a, F: 'static, A, B: 'a, K> KleisliExt<'a, F, A, B> for K
@@ -41,11 +58,8 @@ impl<'a, F: 'static, A, B: 'a> KleisliT<'a, F, A, B> for Kleisli<'a, F, A, B> {
     }
 }
 
-pub struct Compose<'a, F: 'static, A: 'a, B: 'a, Z: 'a, K1, K2>
-where
-    K1: KleisliT<'a, F, A, B>,
-    K2: KleisliT<'a, F, Z, A>,
-{
+/// Compose
+pub struct Compose<'a, F: 'static, A: 'a, B: 'a, Z: 'a, K1, K2> {
     k1: K1,
     k2: K2,
     marker: PhantomData<&'a (F, A, B, Z)>,
@@ -62,17 +76,23 @@ where
     }
 }
 
-fn compose<'a, F: 'static, A: 'a, B: 'a, Z: 'a>(
-    k1: impl KleisliT<'a, F, A, B>,
-    k2: impl KleisliT<'a, F, Z, A>,
-) -> impl KleisliT<'a, F, Z, B>
+/// Map
+pub struct Map<B, K1, Func> {
+    k: K1,
+    func: RcFn<Func>,
+    marker: PhantomData<*const B>,
+}
+
+impl<'a, F, A, B, Z, K1, Func> KleisliT<'a, F, A, Z> for Map<B, K1, Func>
 where
-    F: Monad<'a, F>,
+    B: 'a,
+    Z: 'a,
+    F: Functor<'a, F> + 'static,
+    K1: KleisliT<'a, F, A, B> + 'a,
+    RcFn<Func>: Fn(B) -> Z + 'a,
 {
-    Compose {
-        k1,
-        k2,
-        marker: PhantomData,
+    fn run(&self, a: A) -> Lifted<'a, F, Z> {
+        F::map(self.k.run(a), self.func.clone())
     }
 }
 
@@ -82,6 +102,42 @@ fn apply<'a, F: 'static, A, B: 'a>(
     Kleisli(Box::new(run))
 }
 
+pub struct RcFn<F>(Rc<F>);
+
+impl<F> Clone for RcFn<F> {
+    fn clone(&self) -> Self {
+        RcFn(self.0.clone())
+    }
+}
+
+impl<'a, A, B, F> Fn<(A,)> for RcFn<F>
+where
+    F: Fn(A) -> B + 'a,
+{
+    extern "rust-call" fn call(&self, args: (A,)) -> Self::Output {
+        self.0.call(args)
+    }
+}
+
+impl<'a, A, B, F> FnOnce<(A,)> for RcFn<F>
+where
+    F: Fn(A) -> B + 'a,
+{
+    type Output = B;
+
+    extern "rust-call" fn call_once(self, args: (A,)) -> Self::Output {
+        self.0.call(args)
+    }
+}
+
+impl<'a, A, B, F> FnMut<(A,)> for RcFn<F>
+where
+    F: Fn(A) -> B + 'a,
+{
+    extern "rust-call" fn call_mut(&mut self, args: (A,)) -> Self::Output {
+        self.0.call(args)
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -91,7 +147,7 @@ mod tests {
 
     #[test]
     fn test_compose() {
-        let parse = kleisli::apply(|s: String| {
+        let parse = kleisli::apply(|s: &str| {
             match s.parse::<i32>() {
                 Ok(i) => Some(i),
                 Err(_) => None,
@@ -103,14 +159,11 @@ mod tests {
 
         let parse_and_recriprocal = reciprocal.compose(parse);
 
-        assert_eq!(
-            parse_and_recriprocal.run("123".to_owned()).unlift(),
-            Some(0.008130081)
-        );
+        assert_eq!(parse_and_recriprocal.run("123").unlift(), Some(0.008130081));
 
-        assert_eq!(
-            parse_and_recriprocal.run("yospos".to_owned()).unlift(),
-            None
-        )
+        assert_eq!(parse_and_recriprocal.run("yospos").unlift(), None);
+
+        let doubled = parse_and_recriprocal.map(|f| f * 2 as f32);
+        assert_eq!(doubled.run("123").unlift(), Some(0.016260162))
     }
 }
